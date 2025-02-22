b55b38742767850468db191206ec06326dac171e
Karthickramasamy007 <10cse61@gmail.com>
2025-02-22 21:35:34 +0000

    init

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "google" {
+  project     = "<YOUR_PROJECT_ID>"  # Replace with your Google Cloud project ID
+  region      = "us-central1"        # Choose the region for your bucket
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  name          = "my-unique-bucket-name"  # Ensure this name is globally unique
+  location      = "europe-west1"
+  storage_class = "STANDARD"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true  # Prevent accidental destruction of the bucket
+  }
+}
